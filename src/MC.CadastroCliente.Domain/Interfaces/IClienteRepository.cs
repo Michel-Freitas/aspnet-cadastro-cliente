1605ea5a7911d3c188a2be44578477d0f0400f5a
michel.santos <michelfreitas85@outlook.com>
2021-04-22 11:21:47 -0300

    Criando a classe de Repository generica e Repository para Cliente na Domain

---
+++ src/MC.CadastroCliente.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using MC.CadastroCliente.Domain.Models;
+using System.Collections.Generic;
+
+namespace MC.CadastroCliente.Domain.Interfaces
+{
+    public interface IClienteRepository : IRepository<Cliente>
+    {
+        Cliente ObterPorCpf(string cpf);
+
+        Cliente ObterPorEmail(string email);
+
+        IEnumerable<Cliente> ObterAtivos();
+    }
+}
---
+++ src/MC.CadastroCliente.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using MC.CadastroCliente.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MC.CadastroCliente.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        TEntity Adicionar(TEntity obj);
+
+        TEntity ObterPorId(Guid id);
+
+        IEnumerable<TEntity> ObterTodos();
+
+        IEnumerable<TEntity> ObterTodosPaginado(int s, int t);
+
+        TEntity Atualizar(TEntity obj);
+
+        void Remover(Guid id);
+
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+
+        int SaveChanges();
+    }
+}
