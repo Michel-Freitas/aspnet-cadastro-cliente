e1c2f09dacec1678a75e09d7b6d260f6bc623a79
michel.santos <michelfreitas85@outlook.com>
2021-04-23 08:53:18 -0300

    Criando as classes da ViewModel

---
+++ src/MC.CadastroCliente.Application/ViewModel/ClienteEnderecoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.CadastroCliente.Application.ViewModel
+{
+    public class ClienteEnderecoViewModel
+    {
+        public ClienteViewModel Cliente { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+    }
+}
