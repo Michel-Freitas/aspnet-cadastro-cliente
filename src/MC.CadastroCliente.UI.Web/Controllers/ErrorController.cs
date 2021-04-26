c358fe7f35a2b09d79eb1c5f095366dc33c9d1af
michel.santos <michelfreitas85@outlook.com>
2021-04-26 16:23:41 -0300

    Fazendo algumas estilizações

---
+++ src/MC.CadastroCliente.UI.Web/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MC.CadastroCliente.UI.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        
+        public ActionResult Index(int? code)
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            return View("NotFound");
+        }
+
+        public ActionResult AcessDenied()
+        {
+            return View("AcessDenied");
+        }
+    }
+}
